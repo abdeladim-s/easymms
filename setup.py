5d2ea3ad0dcf3eb994095725b580a206c8a0529b
abdeladim-s <sadiki.abdeladim@gmail.com>
2023-05-29 20:42:07 -0400

    update status

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="easymms",
-    version="0.1.2",
+    version="0.1.3",
     author="Abdeladim Sadiki",
     description="A simple Python package to easily use Meta's Massively Multilingual Speech (MMS) project",
     long_description=long_description,
